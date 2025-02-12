d17d6650a072d8d3513a9ed0e553a7881dd53c7b
Oleh Svyrydov <oleh.svyrydov@yunextraffic.com>
2025-02-12 21:26:05 +0000

    [Sprint 4]My blog in Spring boot. Init commit

---
+++ myblog-site/src/main/java/org/javaprojects/myblogsite/MyblogSiteApplication.java
@@ -0,0 +1,13 @@
+package org.javaprojects.myblogsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogSiteApplication.class, args);
+	}
+
+}
